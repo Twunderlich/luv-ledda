e37ab4c5370833bccf5fdf9d41ce05dd89529153
twunderlich <wunderlich.travis@gmail.com>
2018-03-22 22:55:26 -0500

    cleaned up start-game button, implemented actual createRound

--- page.js
+++ page.js
@@ -1,13 +1,11 @@
 
+
+
 let bt_start_game = document.querySelector( "#start-game" );
-console.log( bt_start_game )
+
 bt_start_game.addEventListener( "click", () => {
-  let round = JSON.stringify( createRound() );
+  let round = JSON.stringify( createRound( DECKS[ 0 ].recipe, PLAYERS ) );
   localStorage.setItem( 'ROUND', round )
   bt_start_game.setAttribute( "display", "none" );
-  console.log( JSON.parse( localStorage.getItem( 'ROUND' ) ) );
 } );
   
-function createRound() {
-  return { round: 'test' };
-}
