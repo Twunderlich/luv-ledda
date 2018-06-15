ea852ab4bfb221ee9d0e9daf83dd6a2cd08da91d
travis wunderlich <wunderlich.travis@gmail.com>
2018-06-15 10:44:33 -0500

    upgrade connection to websocket

--- luvServer.js
+++ luvServer.js
@@ -1,17 +1,27 @@
 'use strict'
 
-const http = require( 'http' );
+
+const express = require( 'express' );
+const app = express(); 
+const http = require( 'http' ).Server( app );
+const io = require( 'socket.io' )( http )
 const path = require( 'path' );
 const url = require( 'url' );
 const fs = require( 'fs' );
+
 const port = process.argv[ 2 ] || 9090;
 
-const app = http.createServer( ( req, res ) => {
-	console.log( `${ new Date() } Received a ${ req.method } request for ${ req.url }` );
+app.get( '/', ( req, res ) => {
+  console.log( req.url );
+  res.sendFile( `${ __dirname }/index.html` );
+} );
 
-  	const parsedUrl = url.parse( req.url, true );
-	let pathname = `.${ parsedUrl.pathname }`;
-	const mimeType = {
+app.use( ( req, res ) => {
+  console.log( `${ new Date() } Received a ${ req.method } request for ${ req.url }` );
+
+  const parsedUrl = url.parse( req.url, true );
+  let pathname = `.${ parsedUrl.pathname }`;
+  const mimeType = {
     '.ico': 'image/x-icon',
     '.html': 'text/html',
     '.js': 'text/javascript',
@@ -30,32 +40,81 @@ const app = http.createServer( ( req, res ) => {
 
   // fs.exists is deprecated
   fs.exists( pathname, function ( exist ) {
-  	if ( !exist ) {
-  		res.statusCode = 404;
-  		res.end( `File ${ pathname } not found`)
-  	}
+    if ( !exist ) {
+      res.statusCode = 404;
+      res.end( `File ${ pathname } not found`)
+    }
+  } );
 
-  	if ( fs.statSync( pathname ).isDirectory() ) {
-  		pathname += '/index.html';
-  	}
-
-  	fs.readFile( pathname, function( err, data ) {
-  		if ( err ) {
-  			res.statusCode = 500;
-  			res.end( `Error getting the file: ${ err }.`)
-  		} else {
-  			const ext = path.parse( pathname ).ext;
-  			res.setHeader( 'Content-Type', mimeType[ ext ] || 'text/plain' );
-  			res.end( data );
-  		}
-  	} );
+  fs.readFile( pathname, function( err, data ) {
+    if ( err ) {
+      res.statusCode = 500;
+      res.end( `Error getting the file: ${ err }.`)
+    } else {
+      const ext = path.parse( pathname ).ext;
+      res.setHeader( 'Content-Type', mimeType[ ext ] || 'text/plain' );
+      res.end( data );
+    }
   } );
 } );
 
-app.listen( port, err => {
-	if ( err ) {
-		return console.log( 'whoops', err );
-	}
-	console.log( `${ new Date() } Luv Ledda Server listening on port: ${ port }` );
-} ); 
+io.on( 'connection' , socket => {
+  console.log( `${ socket.id } connected` );
+} );
+
+http.listen( port, () => {
+  console.log( `listening on *:${ port }` );
+} )
+// const app = http.createServer( ( req, res ) => {
+// 	console.log( `${ new Date() } Received a ${ req.method } request for ${ req.url }` );
+
+//   	const parsedUrl = url.parse( req.url, true );
+// 	let pathname = `.${ parsedUrl.pathname }`;
+// 	const mimeType = {
+//     '.ico': 'image/x-icon',
+//     '.html': 'text/html',
+//     '.js': 'text/javascript',
+//     '.json': 'application/json',
+//     '.css': 'text/css',
+//     '.png': 'image/png',
+//     '.jpg': 'image/jpeg',
+//     '.wav': 'audio/wav',
+//     '.mp3': 'audio/mpeg',
+//     '.svg': 'image/svg+xml',
+//     '.pdf': 'application/pdf',
+//     '.doc': 'application/msword',
+//     '.eot': 'appliaction/vnd.ms-fontobject',
+//     '.ttf': 'aplication/font-sfnt'
+//   };
+
+//   // fs.exists is deprecated
+//   fs.exists( pathname, function ( exist ) {
+//   	if ( !exist ) {
+//   		res.statusCode = 404;
+//   		res.end( `File ${ pathname } not found`)
+//   	}
+
+//   	if ( fs.statSync( pathname ).isDirectory() ) {
+//   		pathname += '/index.html';
+//   	}
+
+//   	fs.readFile( pathname, function( err, data ) {
+//   		if ( err ) {
+//   			res.statusCode = 500;
+//   			res.end( `Error getting the file: ${ err }.`)
+//   		} else {
+//   			const ext = path.parse( pathname ).ext;
+//   			res.setHeader( 'Content-Type', mimeType[ ext ] || 'text/plain' );
+//   			res.end( data );
+//   		}
+//   	} );
+//   } );
+// } );
+
+// app.listen( port, err => {
+// 	if ( err ) {
+// 		return console.log( 'whoops', err );
+// 	}
+// 	console.log( `${ new Date() } Luv Ledda Server listening on port: ${ port }` );
+// } ); 
 
